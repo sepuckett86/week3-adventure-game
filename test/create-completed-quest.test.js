79881410a019ec512898cc4574c1f68904069e24
sepuckett86 <sepuckett86@gmail.com>
2019-05-08 17:02:06 -0700

    filled out test for createCompletedQuest

--- test/create-completed-quest.test.js
+++ test/create-completed-quest.test.js
@@ -1,8 +1,16 @@
 const test = QUnit.test;
 
-test('test', assert => {
+QUnit.module('create completed quest');
+
+test('returns a span element', assert => {
     // Arrange
+    const quest = {
+        title: 'Quest 1'
+    };
+    const expected = '<span class="completed">Quest 1</span>'
     // Act
+    const span = createCompletedQuest(quest);
+    const actual = span.outerHTML;
     // Assert
-    assert.equal(true, true);
+    assert.equal(actual, expected);
 });
