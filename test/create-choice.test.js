149fa8106b8e840f854862c60e6e15d34fa5859d
sepuckett86 <sepuckett86@gmail.com>
2019-05-09 11:51:10 -0700

    add choice-radio function

--- test/create-choice.test.js
+++ test/create-choice.test.js
@@ -1,8 +1,32 @@
 const test = QUnit.test;
 
+function createChoiceRadio(choice) {
+    const div = document.createElement('div');
+    const input = document.createElement('input');
+    const label = document.createElement('label');
+
+    input.id = choice.id;
+    input.type = 'radio';
+    input.name = 'choice-radio';
+    input.value = choice.id;
+    input.required = true;
+
+    label.setAttribute('for', choice.id);
+    label.textContent = choice.description;
+
+    div.appendChild(input);
+    div.appendChild(label);
+
+    return div;
+}
+
 test('create choice radio', assert => {
     // Arrange
-    const expected = '<div><input id="choice" type="radio" name="choice-radio" value="choice1" required><label for="choice">Choice</label></div>'; 
+    const choice = {
+        id: 'choice1',
+        description: 'Description'
+    };
+    const expected = '<div><input id="choice1" type="radio" name="choice-radio" value="choice1" required=""><label for="choice1">Description</label></div>'; 
     // Act
     const div = createChoiceRadio(choice);
     // Assert
