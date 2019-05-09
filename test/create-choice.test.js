6f98bde52b818baee0d450ee715415c25aa742a6
sepuckett86 <sepuckett86@gmail.com>
2019-05-09 11:53:15 -0700

    separate create choice function

--- test/create-choice.test.js
+++ test/create-choice.test.js
@@ -1,24 +1,6 @@
+import createChoice from '../src/quest/create-choice.js';
+
 const test = QUnit.test;
-
-function createChoiceRadio(choice) {
-    const div = document.createElement('div');
-    const input = document.createElement('input');
-    const label = document.createElement('label');
-
-    input.id = choice.id;
-    input.type = 'radio';
-    input.name = 'choice-radio';
-    input.value = choice.id;
-    input.required = true;
-
-    label.setAttribute('for', choice.id);
-    label.textContent = choice.description;
-
-    div.appendChild(input);
-    div.appendChild(label);
-
-    return div;
-}
 
 test('create choice radio', assert => {
     // Arrange
@@ -28,7 +10,7 @@ test('create choice radio', assert => {
     };
     const expected = '<div><input id="choice1" type="radio" name="choice-radio" value="choice1" required=""><label for="choice1">Description</label></div>'; 
     // Act
-    const div = createChoiceRadio(choice);
+    const div = createChoice(choice);
     // Assert
     assert.equal(div.outerHTML, expected);
 });
