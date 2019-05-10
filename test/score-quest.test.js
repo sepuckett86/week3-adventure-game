7228b5cd4f9553e007e5c1f5815ef9a1d4744976
sepuckett86 <sepuckett86@gmail.com>
2019-05-10 09:33:27 -0700

    add another test for scorequest

--- test/score-quest.test.js
+++ test/score-quest.test.js
@@ -2,6 +2,8 @@ import scoreQuest from '../src/quest/score-quest.js';
 
 const test = QUnit.test;
 
+QUnit.module('score quest');
+
 test('returns user with adjusted energy', assert => {
     // Arrange
     const user = {
@@ -15,6 +17,26 @@ test('returns user with adjusted energy', assert => {
     const expected = {
         energy: 40, 
         happiness: 0
+    };
+    // Act
+    const actual = scoreQuest(user, choice);
+    // Assert
+    assert.deepEqual(actual, expected);
+});
+
+test('returns user with adjusted happiness', assert => {
+    // Arrange
+    const user = {
+        energy: 50,
+        happiness: 10
+    };
+    const choice = {
+        energy: 0,
+        happiness: 20
+    };
+    const expected = {
+        energy: 50, 
+        happiness: 30
     };
     // Act
     const actual = scoreQuest(user, choice);
