157994ef158bec9721cbba6fa4acfe3d1fc7f1d4
sepuckett86 <sepuckett86@gmail.com>
2019-05-10 10:14:12 -0700

    add findById function

--- src/find-by-id.js
+++ src/find-by-id.js
@@ -1,5 +1,11 @@
-function findById(object, id) {
-    
+function findById(id, objectList) {
+    for(let i = 0; i < objectList.length; id++) {
+        const object = objectList[i];
+        if(object.id === id) {
+            return object;
+        }
+    }
+    return null;
 }
 
 export default findById;
--- test/find-by-id.test.js
+++ test/find-by-id.test.js
@@ -1,3 +1,5 @@
+import findById from '../src/find-by-id.js';
+
 const test = QUnit.test;
 
 QUnit.module('find by id');
@@ -8,9 +10,9 @@ test('return object with correct id', assert => {
         { id: 'object2' },
         { id: 'object3' }
     ];
-    
+
     const id = 'object1';
     const expected = { id: 'object1' };
     const actual = findById(id, objectList);
-    assert.equal(actual, expected);
+    assert.deepEqual(actual, expected);
 });
