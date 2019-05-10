0e01be0cec1b7ec7d509f48d562b61e87992a1b4
sepuckett86 <sepuckett86@gmail.com>
2019-05-10 16:07:27 -0700

    add has completed all quests function test

---
+++ test/has-completed-all-quests.test.js
@@ -0,0 +1,22 @@
+const test = QUnit.test;
+
+QUnit.module('has completed all quests');
+
+test('return true if completed all quests', assert => {
+    // arrange
+    const questList = [{ id: 'quest1' }, { id: 'quest2' }, { id: 'quest3' }];
+    const user = {
+        completed: {
+            quest1: true,
+            quest2: true,
+            quest3: true
+        }
+    };
+    const expected = true;
+
+    // act
+    const actual = hasCompletedAllQuests(questList, user);
+
+    // assert
+    assert.equal(actual, expected);
+});
--- test/tests.js
+++ test/tests.js
@@ -5,4 +5,4 @@ import './create-completed-quest.test.js';
 import './create-choice.test.js';
 import './score-quest.test.js';
 import './find-by-id.test.js';
-
+import './has-completed-all-quests.test.js';
