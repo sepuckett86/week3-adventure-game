0936f81ac3a4923eec11c88af99442dbdf0f8543
sepuckett86 <sepuckett86@gmail.com>
2019-05-11 19:29:44 -0700

    add score happiness test

--- test/score-happiness.test.js
+++ test/score-happiness.test.js
@@ -2,6 +2,11 @@ const test = QUnit.test;
 
 QUnit.module('score happiness');
 
-test('test', assert => {
-    assert.equal(true, true);
+test('return miserable/content/ecstatic if 0 happiness', assert => {
+    const user = {
+        happiness: 0
+    };
+    const expected = 'miserable';
+    const actual = scoreHappiness(user);
+    assert.equal(actual, expected);
 });
