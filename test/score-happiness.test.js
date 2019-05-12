b0aa8692799a8c3f9780ca660d0e2ce8212d74ab
sepuckett86 <sepuckett86@gmail.com>
2019-05-11 19:33:58 -0700

    add score happiness function and more tests

--- test/score-happiness.test.js
+++ test/score-happiness.test.js
@@ -1,12 +1,32 @@
+import scoreHappiness from '../src/end/score-happiness.js';
+
 const test = QUnit.test;
 
 QUnit.module('score happiness');
 
-test('return miserable/content/ecstatic if 0 happiness', assert => {
+test('return miserable if 0 happiness', assert => {
     const user = {
         happiness: 0
     };
     const expected = 'miserable';
+    const actual = scoreHappiness(user);
+    assert.equal(actual, expected);
+});
+
+test('return content if greater than 0 and less than 25 happiness', assert => {
+    const user = {
+        happiness: 24
+    };
+    const expected = 'content';
+    const actual = scoreHappiness(user);
+    assert.equal(actual, expected);
+});
+
+test('return ecstatic if 25 or greater happiness', assert => {
+    const user = {
+        happiness: 25
+    };
+    const expected = 'ecstatic';
     const actual = scoreHappiness(user);
     assert.equal(actual, expected);
 });
