6dde3ad5ba29512ce86e9da2a0a890564ac77473
sepuckett86 <sepuckett86@gmail.com>
2019-05-11 20:00:55 -0700

    wire up ending

--- src/end/end.js
+++ src/end/end.js
@@ -1,18 +1,44 @@
 import energyOutcomes from './energy-outcomes.js';
 import happinessOutcomes from './happiness-outcomes.js';
 import loadProfile from '../load-profile.js';
+import isDepleted from '../map/is-depleted.js';
+import hasCompletedAllQuests from '../map/has-completed-all-quests.js';
+import scoreEnergy from './score-energy.js';
+import scoreHappiness from './score-happiness.js';
+import api from '../services/api.js';
 
 const endMessageElement = document.getElementById('end-message');
 const endImageElement = document.getElementById('end-image');
 
-const endDepletedMessage = `You did not make it 
-    through the night and head in early to bed. ` + 
-    energyOutcomes['shattered'] + ' ' + happinessOutcomes['ecstatic'];
-const endEnergizedMessage = `You made it with 
-    energy to spare! ` + 
-    energyOutcomes['tired'] + ' ' + happinessOutcomes['ecstatic'];
+const user = api.getUser();
+const energy = scoreEnergy(user);
+const happiness = scoreHappiness(user);
 
-endMessageElement.textContent = endEnergizedMessage || endDepletedMessage;
-endImageElement.src = './assets/bed.jpg' || './assets/rain.jpg';
+let endMessage = '';
+if(hasCompletedAllQuests(user)) {
+    endMessage = `You made it to all parts of the house 
+    during your party and head to the bedroom for a well-deserved rest. ` + 
+    energyOutcomes[energy] + ' ' + happinessOutcomes[happiness];
+} else if(isDepleted(user)) {
+    endMessage = `You did not make it 
+    through the night and head in early to bed. ` + 
+    energyOutcomes[energy] + ' ' + happinessOutcomes[happiness];
+} else {
+    endMessage = `You made it with 
+    energy to spare! ` + 
+    energyOutcomes[energy] + ' ' + happinessOutcomes[happiness];
+}
+endMessageElement.textContent = endMessage;
+
+
+if(happiness === 'miserable') {
+    endImageElement.src = './assets/rain.jpg';
+} else {
+    endImageElement.src = './assets/bed.jpg';
+}
+
+
+
+
 
 loadProfile();
--- src/end/happiness-outcomes.js
+++ src/end/happiness-outcomes.js
@@ -1,7 +1,7 @@
 const happinessOutcomes = {
-    miserable: 'Mood-wise, you have been surrounded by family and friends all night, yet you are miserable. You need to try harder.',
+    miserable: 'You have been surrounded by family and friends all night, yet you are miserable. Sad times.',
     content: 'Mood-wise, you are content with how the evening went, regardless of energy level. You consider this a win.',
-    ecstatic: 'Mood-wise, you are ecstatic to have spent a wonderful night with family and friends. Go you!'
+    ecstatic: 'Mood-wise, you are ecstatic to have spent a wonderful night with family and friends, and you have a gigantic smile on your face. Go you!'
 };
 
 export default happinessOutcomes;
