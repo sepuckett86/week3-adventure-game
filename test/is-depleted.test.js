4ba25ef2a066f80ab846a8d3f11729f30fd58901
sepuckett86 <sepuckett86@gmail.com>
2019-05-11 18:59:42 -0700

    add function and more tests for is depleted

--- test/is-depleted.test.js
+++ test/is-depleted.test.js
@@ -2,11 +2,33 @@ const test = QUnit.test;
 
 QUnit.module('is depleted');
 
+function isDepleted(user) {
+    return user.energy <= 0;
+}
+
 test('return true if energy below 0', assert => {
     const user = {
         energy: -10
-    }
+    };
     const expected = true;
+    const actual = isDepleted(user);
+    assert.equal(actual, expected);
+});
+
+test('return true if energy equal to 0', assert => {
+    const user = {
+        energy: 0
+    };
+    const expected = true;
+    const actual = isDepleted(user);
+    assert.equal(actual, expected);
+});
+
+test('return false if energy above', assert => {
+    const user = {
+        energy: 10
+    };
+    const expected = false;
     const actual = isDepleted(user);
     assert.equal(actual, expected);
 });
