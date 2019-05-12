6e354acc512dcd747083406b342a645ee71000c1
sepuckett86 <sepuckett86@gmail.com>
2019-05-11 19:27:29 -0700

    add score energy function and more tests

--- test/score-energy.test.js
+++ test/score-energy.test.js
@@ -1,18 +1,32 @@
+import scoreEnergy from '../src/end/score-energy.js';
+
 const test = QUnit.test;
 
 QUnit.module('score energy');
 
-function scoreEnergy(user) {
-    if(user.energy < 15) {
-        return 'shattered';
-    }
-}
-
 test('return shattered if under 15 energy', assert => {
     const user = {
-        energy: 0
+        energy: 14
     };
     const expected = 'shattered';
+    const actual = scoreEnergy(user);
+    assert.equal(actual, expected);
+});
+
+test('return tired if under 30 energy but 15 or higher', assert => {
+    const user = {
+        energy: 15
+    };
+    const expected = 'tired';
+    const actual = scoreEnergy(user);
+    assert.equal(actual, expected);
+});
+
+test('return energetic if 30 or higher energy', assert => {
+    const user = {
+        energy: 30
+    };
+    const expected = 'energetic';
     const actual = scoreEnergy(user);
     assert.equal(actual, expected);
 });
