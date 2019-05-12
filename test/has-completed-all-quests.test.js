480a3822b490f1308fd01ace27318d891c22bc5c
sepuckett86 <sepuckett86@gmail.com>
2019-05-11 18:52:49 -0700

    add hasCompletedAllQuests function and another test and move to separate file

--- test/has-completed-all-quests.test.js
+++ test/has-completed-all-quests.test.js
@@ -1,3 +1,5 @@
+import hasCompletedAllQuests from '../src/map/has-completed-all-quests.js';
+
 const test = QUnit.test;
 
 QUnit.module('has completed all quests');
@@ -13,6 +15,24 @@ test('return true if completed all quests', assert => {
         }
     };
     const expected = true;
+
+    // act
+    const actual = hasCompletedAllQuests(questList, user);
+
+    // assert
+    assert.equal(actual, expected);
+});
+
+test('return false if quests not all completed', assert => {
+    // arrange
+    const questList = [{ id: 'quest1' }, { id: 'quest2' }, { id: 'quest3' }];
+    const user = {
+        completed: {
+            quest1: true,
+            quest2: true
+        }
+    };
+    const expected = false;
 
     // act
     const actual = hasCompletedAllQuests(questList, user);
